029c8ff6b076683575259054e99b9c8d3b70a47c
Ângel Cagiza <angelcagiza@gmail.com>
2025-05-18 23:54:01 +0100

    implement search toggle and drawer menu in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,113 @@
+import { useState } from "react";
 import "./App.css";
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from "./components/ui/drawer";
 
 function App() {
+	const [isSearchVisible, setIsSearchVisible] = useState(false);
+
+	function toggleSearch() {
+		setIsSearchVisible(!isSearchVisible);
+	}
 	return (
 		<>
 			<header className="">
 				<nav className="flex justify-between mt-5 px-6 items-center lg:w-10/12">
-					<div id="logo" className="">
-						<img
-							className="sm:w-44 lg:w-60"
-							src="assets/images/Logo.svg"
-							alt=""
-						/>
-					</div>
-
-					<search className="justify-between w-full hidden sm:flex sm:w-7/12 lg:w-5/12">
-						<form className="w-full hidden sm:block">
-							<input
-								placeholder="Pesquisar"
-								className="h-11 w-11/12 rounded-xl bg-slate-700 pl-5 focus:outline-none placeholder:font-semibold placeholder:opacity-50 placeholder:text-white text-white lg:h-16"
-								type="search"
-								id="searchInput"
-								name="q"
-							/>
-						</form>
-
-						<button id="closeBtn" className="hidden">
+					{!isSearchVisible && (
+						<div className="">
 							<img
-								className="size-6"
-								src="assets/images/x.svg"
+								className="sm:w-44 lg:w-60"
+								src="assets/images/Logo.svg"
 								alt=""
 							/>
-						</button>
-					</search>
+						</div>
+					)}
 
-					<div
-						id="btns"
-						className="flex justify-between w-20 items-center sm:w-fit"
-					>
-						<button id="searchBtn" className="sm:hidden">
-							<img
-								className="size-6"
-								src="assets/images/magGlass.svg"
-								alt=""
-							/>
-						</button>
+					{isSearchVisible && (
+						<search className="justify-between w-full flex sm:w-7/12 lg:w-5/12">
+							<form className="w-full sm:block">
+								<input
+									placeholder="Pesquisar"
+									className="h-11 w-11/12 rounded-xl bg-slate-700 pl-5 focus:outline-none placeholder:font-semibold placeholder:opacity-50 placeholder:text-white text-white lg:h-16"
+									type="search"
+									id="searchInput"
+									name="q"
+								/>
+							</form>
 
-						<button className="lg:hidden">
-							<img
-								className="size-6"
-								src="assets/images/menu.svg"
-								alt=""
-							/>
-						</button>
-					</div>
+							<button id="closeBtn" onClick={toggleSearch}>
+								<img
+									className="size-6"
+									src="assets/images/x.svg"
+									alt=""
+								/>
+							</button>
+						</search>
+					)}
+					<form className="w-full hidden sm:flex sm:w-7/12 lg:w-5/12">
+								<input
+									placeholder="Pesquisar"
+									className="h-11 w-11/12 rounded-xl bg-slate-700 pl-5 focus:outline-none placeholder:font-semibold placeholder:opacity-50 placeholder:text-white text-white lg:h-20 lg:placeholder:text-2xl lg:text-2xl"
+									type="search"
+									id="searchInput"
+									name="q"
+								/>
+							</form>
+                    
+
+                    {!isSearchVisible && (
+                        <div
+                            id="btns"
+                            className="flex justify-between w-20 items-center sm:w-fit"
+                        >
+                            <button
+                                id="searchBtn"
+                                className="sm:hidden"
+                                onClick={toggleSearch}
+                            >
+                                <img
+                                    className="size-6"
+                                    src="assets/images/magGlass.svg"
+                                    alt=""
+                                />
+                            </button>
+
+                            {/* <button className="lg:hidden">
+                                <img
+                                    className="size-6"
+                                    src="assets/images/menu.svg"
+                                    alt=""
+                                />
+                            </button> */}
+
+							<Drawer>
+								<DrawerTrigger asChild>
+									<button className="lg:hidden">
+										<img
+											className="size-6"
+											src="assets/images/menu.svg"
+											alt=""
+										/>
+									</button>
+								</DrawerTrigger>
+								<DrawerContent>
+									<div className="mx-auto w-full max-w-sm">
+										<DrawerHeader>
+											<DrawerTitle>Menu</DrawerTitle>
+										</DrawerHeader>
+										<div className="p-4 pb-0">
+											<ul className="flex flex-col gap-4">
+												<li>Home</li>
+												<li>Sobre</li>
+												<li>Contato</li>
+												<li>Ajuda</li>
+											</ul>
+										</div>
+									</div>
+								</DrawerContent>
+							</Drawer>
+                        </div>
+                    )}
+
 				</nav>
 			</header>
 		</>
