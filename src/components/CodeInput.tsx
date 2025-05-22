c4930209c7c664087357b94de47466d08a8fc58a
Ângel Cagiza <angelcagiza@gmail.com>
2025-05-22 22:28:33 +0100

    remove text area resizer

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -4,7 +4,7 @@ function CodeInput() {
 			<div className="bg-black w-10/12 h-11/12 rounded-xl  p-5">
 				<textarea
 					placeholder="Write your code here"
-					className="w-full h-full focus:outline-none placeholder:font-semibold placeholder:opacity-50 placeholder:text-white text-white"
+					className="w-full h-full resize-none focus:outline-none placeholder:font-semibold placeholder:opacity-50 placeholder:text-white text-white"
 					name=""
 					id=""
 				></textarea>
