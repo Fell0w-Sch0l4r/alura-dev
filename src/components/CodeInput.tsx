cca397ee876b73659637fbb95e3faa0bbe8ce9db
Ângel Cagiza <angelcagiza@gmail.com>
2025-05-22 22:17:10 +0100

    feat(CodeInput): add placeholder and enhance textarea styling

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -1,11 +1,16 @@
 function CodeInput() {
-  return (
-    <div className=" bg-sky-300 rounded-xl h-96 flex items-center justify-center">
-      <div className="bg-black w-10/12 h-10/12 rounded-xl  p-5">
-        <textarea className="w-full h-full focus:outline-none" name="" id=""></textarea>
-      </div>
-    </div>
-  );
+	return (
+		<div className=" bg-sky-300 rounded-xl h-96 flex items-center justify-center ">
+			<div className="bg-black w-10/12 h-10/12 rounded-xl  p-5">
+				<textarea
+					placeholder="Write your code here"
+					className="w-full h-full focus:outline-none placeholder:font-semibold placeholder:opacity-50 placeholder:text-white text-white"
+					name=""
+					id=""
+				></textarea>
+			</div>
+		</div>
+	);
 }
 
 export default CodeInput;
