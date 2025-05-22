92b4e0e8ddbe7bba873f142f8a6c6033946a59d7
Ângel Cagiza <angelcagiza@gmail.com>
2025-05-22 22:25:26 +0100

    correct height of container for proper layout

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -1,6 +1,6 @@
 function CodeInput() {
 	return (
-		<div className=" bg-sky-300 rounded-xl h-150 flex items-center justify-center ">
+		<div className=" bg-sky-300 rounded-xl h-100 flex items-center justify-center ">
 			<div className="bg-black w-10/12 h-11/12 rounded-xl  p-5">
 				<textarea
 					placeholder="Write your code here"
