a670a94617af1dbf46fd5a13d91b896973ec4ebe
Ângel Cagiza <angelcagiza@gmail.com>
2025-05-22 22:23:57 +0100

    adjust height of container and inner div for better layout

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -1,7 +1,7 @@
 function CodeInput() {
 	return (
-		<div className=" bg-sky-300 rounded-xl h-96 flex items-center justify-center ">
-			<div className="bg-black w-10/12 h-10/12 rounded-xl  p-5">
+		<div className=" bg-sky-300 rounded-xl h-150 flex items-center justify-center ">
+			<div className="bg-black w-10/12 h-11/12 rounded-xl  p-5">
 				<textarea
 					placeholder="Write your code here"
 					className="w-full h-full focus:outline-none placeholder:font-semibold placeholder:opacity-50 placeholder:text-white text-white"
