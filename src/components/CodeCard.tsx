faaf016ef83d4fb61bdafff2295031cd43d99729
Ângel Cagiza <angelcagiza@gmail.com>
2025-05-28 15:31:08 +0100

    add export image feature

--- src/components/CodeCard.tsx
+++ src/components/CodeCard.tsx
@@ -3,6 +3,8 @@ import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "prismjs/themes/prism-tomorrow.css";
+import { useRef } from "react";
+import {toPng} from "html-to-image"
 
 interface Props {
   backGroundColor: string;
@@ -20,29 +22,46 @@ function CodeCard({
   language,
   title,
 }: Props) {
+
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  const handleDownload = async () => {
+    if (cardRef.current) {
+      const dataUrl = await toPng(cardRef.current);
+      const link = document.createElement("a");
+      link.download = `${title}.png`;
+      link.href = dataUrl;
+      link.click();
+    }
+  };
   return (
     <div className={`bg-slate-950 mb-10 rounded-xl`}>
-       <div
-      className={`${backGroundColor} rounded-xl h-fit flex items-center justify-center mt-5 p-5`}
-    >
-      <div className="w-full  rounded-xl">
-        <SyntaxHighlighter
-          language={language}
-          style={oneDark}
-          showLineNumbers={true}
-        >
-          {code}
-        </SyntaxHighlighter>
+      <div
+        ref={cardRef}
+        className={`${backGroundColor} rounded-xl h-fit flex items-center justify-center mt-5 p-5`}
+      >
+        <div className="w-full  rounded-xl">
+          <SyntaxHighlighter
+            language={language}
+            style={oneDark}
+            showLineNumbers={true}
+          >
+            {code}
+          </SyntaxHighlighter>
+        </div>
       </div>
-      
-    </div>
 
-    <div className={`p-5`}>
-      <h1 className={`text-2xl text-bold mb-5`}>{title}</h1>
-      <p className={``}>{description}</p>
+      <div className={`p-5`}>
+        <h1 className={`text-2xl text-bold mb-5`}>{title}</h1>
+        <p className={``}>{description}</p>
+      </div>
+      <button
+        onClick={handleDownload}
+        className="mt-2 px-4 py-2 bg-blue-600 text-white rounded"
+      >
+        Download as Image
+      </button>
     </div>
-    </div>
-   
   );
 }
 
