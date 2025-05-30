0c1875ffa5752777cb4cb78263744710c13596db
Ângel Cagiza <angelcagiza@gmail.com>
2025-05-30 12:14:28 +0100

    refactor CodeCard component to enable image download functionality

--- src/components/CodeCard.tsx
+++ src/components/CodeCard.tsx
@@ -4,7 +4,7 @@ import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "prismjs/themes/prism-tomorrow.css";
 import { useRef } from "react";
-//import { toPng } from "html-to-image";
+import { toPng } from "html-to-image";
 
 interface Props {
   backGroundColor: string;
@@ -27,44 +27,46 @@ function CodeCard({
 }: Props) {
   const cardRef = useRef<HTMLDivElement>(null);
 
-//   const handleDownload = async () => {
-//     if (cardRef.current) {
-//       const dataUrl = await toPng(cardRef.current);
-//       const link = document.createElement("a");
-//       link.download = `${title}.png`;
-//       link.href = dataUrl;
-//       link.click();
-//     }
-//   };
+  const handleDownload = async () => {
+    if (cardRef.current) {
+      const dataUrl = await toPng(cardRef.current);
+      const link = document.createElement("a");
+      link.download = `${title}.png`;
+      link.href = dataUrl;
+      link.click();
+    }
+  };
 
   return (
-    <div className={`bg-slate-950 mb-10 rounded-xl`} ref={cardRef}>
-      <div
-        className={`${backGroundColor} rounded-xl h-fit flex items-center justify-center mt-5 p-5`}
-      >
-        <div className="w-full  rounded-xl">
-          <SyntaxHighlighter
-            language={language}
-            style={oneDark}
-            showLineNumbers={true}
-          >
-            {code}
-          </SyntaxHighlighter>
+    <div className={`bg-slate-950 mb-10 rounded-xl`}>
+      <div ref={cardRef}>
+        <div
+          className={`${backGroundColor} rounded-xl h-fit flex items-center justify-center mt-5 p-5`}
+        >
+          <div className="w-full  rounded-xl">
+            <SyntaxHighlighter
+              language={language}
+              style={oneDark}
+              showLineNumbers={true}
+            >
+              {code}
+            </SyntaxHighlighter>
+          </div>
+        </div>
+
+        <div className={`p-5`}>
+          <h1 className={`text-2xl text-bold mb-5`}>{title}</h1>
+          <p className={`break-words`}>{description}</p>
         </div>
       </div>
 
-      <div className={`p-5`}>
-        <h1 className={`text-2xl text-bold mb-5`}>{title}</h1>
-        <p className={`break-words`}>{description}</p>
-      </div>
-
       <div className="flex justify-between">
-        {/* <button
+        <button
           onClick={handleDownload}
           className="mt-2 px-4 py-2 bg-blue-600 text-white rounded"
         >
           Download as Image
-        </button> */}
+        </button>
 
         <button
           onClick={() => handleDelete(id)}
