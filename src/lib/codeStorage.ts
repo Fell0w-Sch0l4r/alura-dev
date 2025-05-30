b526842530e6aee6a96068db340363a01c90ce2d
Ângel Cagiza <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2025-05-30 23:53:51 +0100

    change userCodes to a constant

--- src/lib/codeStorage.ts
+++ src/lib/codeStorage.ts
@@ -1,11 +1,11 @@
 import type { Code } from "@/types/code";
 
 export function getCodesFromStorage(): Code[]{
-    let userCodes: Code[] = []
+    const userCodes: Code[];
     const userCodesJson: string | null = localStorage.getItem("userCodes")
 
     if(userCodesJson){
-        userCodes = JSON.parse(userCodesJson)
+   userCodes.push(JSON.parse(userCodesJson))
         return userCodes
     }
 
